ef867ba09de0bf2f61214ff7fc32266cddd5c372
JOAO PEDRO FERNANDES DE ALMEIDA <joao.8976@aluno.pr.senac.br>
2023-04-14 07:38:59 -0300

    ATUALIZAÇÃO

---
+++ exemplo_if.php
@@ -0,0 +1,13 @@
+    <?php
+        #Exemplo de código com IF
+
+        $idade = 30;
+
+        if ($idade <=17){
+            echo("Sua idade é menor de 18 anos");
+        }elseif($idade >=18){
+            echo("Sua idade é maior de 18 anos");
+        }else{
+            echo("Não correspode a uma idade");
+        }
+    ?>
